7cd1be15bb6d8a735a524332394eabf4f811ce0f
archaeogeek <jo.k.cook@gmail.com>
2021-08-29 18:40:45 +0100

    mods to script for creating geonames index

--- create_index.sh
+++ create_index.sh
@@ -1,13 +1,19 @@
+#/bin/bash
+
+REGION=GB
+CONTAINER=elasticsearch
+ESVERSION=6.4.2
+
 echo "Starting Docker container and data volume..."
-sudo docker run -d -p 127.0.0.1:9200:9200 -v $PWD/geonames_index/:/usr/share/elasticsearch/data elasticsearch:5.5.2
+sudo docker run -d -p 9200:9200 -v $PWD/geonames_index/:/usr/share/elasticsearch/data elasticsearch:$ESVERSION
 
 echo "Downloading Geonames gazetteer..."
-wget http://download.geonames.org/export/dump/allCountries.zip
+wget http://download.geonames.org/export/dump/$REGION.zip
 echo "Unpacking Geonames gazetteer..."
-unzip allCountries.zip
+unzip $REGION.zip
 
 echo "Creating mappings for the fields in the Geonames index..."
-curl -XPUT 'localhost:9200/geonames' -H 'Content-Type: application/json' -d @geonames_mapping.json
+curl -XPUT '$CONTAINER:9200/geonames' -H 'Content-Type: application/json' -d @geonames_mapping.json
 
 echo "Loading gazetteer into Elasticsearch..."
 python geonames_elasticsearch_loader.py
