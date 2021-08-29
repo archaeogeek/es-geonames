391590407613c838ac424df3079d5f807740ac01
archaeogeek <jo.k.cook@gmail.com>
2021-08-29 18:47:22 +0100

    unzip should overwrite existing file, need python3

--- create_index.sh
+++ create_index.sh
@@ -3,6 +3,7 @@
 REGION=GB
 CONTAINER=elasticsearch
 ESVERSION=6.4.2
+PYTHONEXE=python3
 
 echo "Starting Docker container and data volume..."
 sudo docker run -d -p 9200:9200 -v $PWD/geonames_index/:/usr/share/elasticsearch/data elasticsearch:$ESVERSION
@@ -10,12 +11,12 @@ sudo docker run -d -p 9200:9200 -v $PWD/geonames_index/:/usr/share/elasticsearch
 echo "Downloading Geonames gazetteer..."
 wget http://download.geonames.org/export/dump/$REGION.zip
 echo "Unpacking Geonames gazetteer..."
-unzip $REGION.zip
+unzip -o $REGION.zip
 
 echo "Creating mappings for the fields in the Geonames index..."
 curl -XPUT '${CONTAINER}:9200/geonames' -H 'Content-Type: application/json' -d @geonames_mapping.json
 
 echo "Loading gazetteer into Elasticsearch..."
-python geonames_elasticsearch_loader.py
+$PYTHONEXE geonames_elasticsearch_loader.py
 
 echo "Done"
